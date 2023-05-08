90d8ab23da1f734e4491a514f297e2d939292fe5
daniel <danielgonzalez252@gmail.com>
2023-05-08 00:48:02 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+
+//First Drivers
+const returnFirstTwoDrivers = function (drivers)
+{
+  return drivers.slice(0,2);
+}
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const firstTwoDrivers = returnFirstTwoDrivers(drivers);
+console.log(firstTwoDrivers);
+
+//Last Drivers
+const returnLastTwoDrivers = function (drivers)
+{
+  return drivers.slice(-2);
+}
+
+const lastTwoDrivers = returnLastTwoDrivers(drivers);
+
+//SelectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//CreateFareMultiplier
+function createFareMultiplier(integer)
+{
+  return function(fare)
+  {
+    return fare * integer;
+  }
+}
+
+//FareDoubler
+const fareDoubler = createFareMultiplier(2);
+
+//FareTripler
+const fareTripler = createFareMultiplier(3);
+
+//selectDifferentDrivers
+function selectDifferentDrivers(drivers, callback)
+{
+  return callback(drivers);
+}
+
+selectDifferentDrivers(drivers,returnFirstTwoDrivers);
+selectDifferentDrivers(drivers,returnLastTwoDrivers);
